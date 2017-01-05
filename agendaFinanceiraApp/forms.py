697baf9a0c3af8cdd4993935aa7288659730b521
Pedro Silva <ph.gsilva@hotmail.com>
2017-01-05 13:37:03 -0200

    adicionado import que faltou

--- agendaFinanceiraApp/forms.py
+++ agendaFinanceiraApp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 from .models import Receita, Despesas
-
+from django.utils.translation import ugettext_lazy as _
 
 class UserForm(forms.ModelForm):
 	class Meta:
