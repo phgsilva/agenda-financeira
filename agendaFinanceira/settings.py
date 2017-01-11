a9c8144158ce79a88fa5fe9f21ba96a8de38ccf8
Pedro Silva <ph.gsilva@hotmail.com>
2017-01-11 21:02:50 -0200

    Correções formato de data

--- agendaFinanceira/settings.py
+++ agendaFinanceira/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
@@ -122,3 +122,11 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = '/menu/' 
 LOGIN_URL = '/login/'
+
+DATE_INPUT_FORMATS = [
+                        "%d/%m/%Y",
+                    ]
+
+DATETIME_INPUT_FORMATS = [
+                            "%d/%m/%Y",
+                        ]
--- agendaFinanceiraApp/views.py
+++ agendaFinanceiraApp/views.py
@@ -1,3 +1,4 @@
+import datetime 
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
@@ -28,30 +29,36 @@ def cadastrar(request):
 # Menu inical apos login
 @login_required
 def menu(request):
-    return render(request, 'agendaFinanceiraApp/menu.html', {})
+	return render(request, 'agendaFinanceiraApp/menu.html', {})
 
 
 # Busca receitas
 @login_required
 def consultaReceita(request):
-	data_inicio = request.GET.get('data_inicio')
-	data_fim = request.GET.get('data_fim')
 	receitas = []
 	
-	if (data_inicio is not None) and (data_fim is not None):
-		receitas = Receita.objects.filter(user=request.user, data_entrada__range=(data_inicio, data_fim)).order_by('data_entrada')
+	if (request.GET.get('data_inicio') is not None) & (request.GET.get('data_fim') is not None):
+		data_inicio = request.GET.get('data_inicio')
+		data_fim = request.GET.get('data_fim')
+
+		data_inicio = datetime.datetime.strptime(data_inicio, "%d/%m/%Y").strftime("%Y-%m-%d %H:%M:%S")
+		data_fim = datetime.datetime.strptime(data_fim, "%d/%m/%Y").strftime("%Y-%m-%d %H:%M:%S")	
+		receitas = Receita.objects.filter(usuario=request.user, data_entrada__range=(data_inicio, data_fim)).order_by('data_entrada')
 	
 	return render(request, 'agendaFinanceiraApp/consultaReceita.html', {'receitas': receitas})
 
 # Busca despesas
 @login_required
 def consultaDespesa(request):
-	data_inicio = request.GET.get('data_inicio')
-	data_fim = request.GET.get('data_fim')
 	despesas = []
 
-	if (data_inicio is not None) and (data_fim is not None):
-		despesas = Despesas.objects.filter(user=request.user, data_vencimento__range=(data_inicio, data_fim)).order_by('data_vencimento')
+	if (request.GET.get('data_inicio') is not None) & (request.GET.get('data_fim') is not None):
+		data_inicio = request.GET.get('data_inicio')
+		data_fim = request.GET.get('data_fim')
+		
+		data_inicio = datetime.datetime.strptime(data_inicio, "%d/%m/%Y").strftime("%Y-%m-%d %H:%M:%S")
+		data_fim = datetime.datetime.strptime(data_fim, "%d/%m/%Y").strftime("%Y-%m-%d %H:%M:%S")	
+		despesas = Despesas.objects.filter(usuario=request.user, data_vencimento__range=(data_inicio, data_fim)).order_by('data_vencimento')
 	
 	return render(request, 'agendaFinanceiraApp/ConsultaDespesa.html', {'despesas': despesas})
 
@@ -64,7 +71,7 @@ def casdatrarReceita(request):
 			receita = receitaForm.save(commit=False)
 			receita.usuario = request.user
 			receita.save()
-			return redirect('agendaFinanceiraApp.views.consultaReceita')
+			return redirect('/consultar/receita/')
 	else:
 		receitaForm = ReceitaForm()
 		return render(request, 'agendaFinanceiraApp/CadastroReceita.html', {'form': receitaForm})
@@ -78,7 +85,7 @@ def casdatrarDespesas(request):
 			despesa = despesaForm.save(commit=False)
 			despesa.usuario = request.user
 			despesa.save()
-			return redirect('agendaFinanceiraApp.views.consultaDespesa')
+			return redirect('/consultar/despesa/')
 	else:
 		despesaForm = DespesaForm()
 		return render(request, 'agendaFinanceiraApp/CadastroDespesa.html', {'form': despesaForm})
