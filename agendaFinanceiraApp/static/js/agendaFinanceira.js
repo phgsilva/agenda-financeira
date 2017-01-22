ebb91ea552323a16d61ddabcd13fd642717ccf66
Pedro Silva <ph.gsilva@hotmail.com>
2017-01-22 21:25:47 -0200

    implementação do javascript

---
+++ agendaFinanceiraApp/static/js/agendaFinanceira.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function () {
+    $("#id_data_vencimento").datepicker({ dateFormat: "dd/mm/yy" });
+    $("#id_data_entrada").datepicker({ dateFormat: "dd/mm/yy" });
+    $("#txbDataInicio").datepicker({ dateFormat: "dd/mm/yy" });
+    $("#txbDataFim").datepicker({ dateFormat: "dd/mm/yy" });
+});
+
+function validarValor() {
+    var valor = parseInt($("#id_valor").value);
+
+    if(isNaN(valor)){
+        alert('O conteúdo para o campo "Valor" deve ser um numero!');
+        return false;
+    }
+    else
+        return true;
+}
+
